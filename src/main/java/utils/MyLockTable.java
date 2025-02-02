5cf4ed0a3c09b9f3c540c26ec8d531dfb4e4c30e
Cristian Cretu <crisemcr@gmail.com>
2025-02-02 14:58:10 +0200

    feat: use synchronized instead of reentrant lock

--- src/main/java/utils/MyLockTable.java
+++ src/main/java/utils/MyLockTable.java
@@ -3,94 +3,56 @@ package utils;
 import exceptions.LockException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MyLockTable implements ILockTable {
   private Map<Integer, Integer> lockTable;
   private int freeLocation;
-  private final ReentrantLock lock;
 
   public MyLockTable() {
     this.lockTable = new HashMap<>();
     this.freeLocation = 0;
-    this.lock = new ReentrantLock();
   }
 
   @Override
-  public int allocate() {
-    lock.lock();
-    try {
-      freeLocation++;
-      lockTable.put(freeLocation, -1);
-      return freeLocation;
-    } finally {
-      lock.unlock();
-    }
+  public synchronized int allocate() {
+    freeLocation++;
+    lockTable.put(freeLocation, -1);
+    return freeLocation;
   }
 
   @Override
-  public boolean isDefined(int location) {
-    lock.lock();
-    try {
-      return lockTable.containsKey(location);
-    } finally {
-      lock.unlock();
-    }
+  public synchronized boolean isDefined(int location) {
+    return lockTable.containsKey(location);
   }
 
   @Override
-  public int getValue(int location) throws LockException {
-    lock.lock();
-    try {
-      if (!isDefined(location)) {
-        throw new LockException("Lock location " + location + " is not defined!");
-      }
-      return lockTable.get(location);
-    } finally {
-      lock.unlock();
+  public synchronized int getValue(int location) throws LockException {
+    if (!isDefined(location)) {
+      throw new LockException("Lock location " + location + " is not defined!");
     }
+    return lockTable.get(location);
   }
 
   @Override
-  public void update(int location, int value) throws LockException {
-    lock.lock();
-    try {
-      if (!isDefined(location)) {
-        throw new LockException("Lock location " + location + " is not defined!");
-      }
-      lockTable.put(location, value);
-    } finally {
-      lock.unlock();
+  public synchronized void update(int location, int value) throws LockException {
+    if (!isDefined(location)) {
+      throw new LockException("Lock location " + location + " is not defined!");
     }
+    lockTable.put(location, value);
   }
 
   @Override
-  public Map<Integer, Integer> getLockTable() {
-    lock.lock();
-    try {
-      return new HashMap<>(lockTable);
-    } finally {
-      lock.unlock();
-    }
+  public synchronized Map<Integer, Integer> getLockTable() {
+    return new HashMap<>(lockTable);
   }
 
   @Override
-  public void setLockTable(Map<Integer, Integer> newLockTable) {
-    lock.lock();
-    try {
-      this.lockTable = new HashMap<>(newLockTable);
-    } finally {
-      lock.unlock();
-    }
+  public synchronized void setLockTable(Map<Integer, Integer> newLockTable) {
+    this.lockTable = new HashMap<>(newLockTable);
   }
 
   @Override
-  public String toString() {
-    lock.lock();
-    try {
-      return lockTable.toString();
-    } finally {
-      lock.unlock();
-    }
+  public synchronized String toString() {
+    return lockTable.toString();
   }
 }
